637e39ef70a362313ff97e5166391022f0f29c7c
Kseniya Halakova <ksenechkasja@mail.ru>
2021-09-03 23:10:42 +0300

    feat: implement solveSudoku function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+function isCorrect(matrix, row, col, x) {
+  for (let i = 0; i < 9; i++) {
+    const a = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+    const b = 3 * Math.floor(col / 3) + (i % 3);
+    if (matrix[row][i] === x || matrix[i][col] === x || matrix[a][b] === x) {
+      return false;
+    }
+  }
+  return true;
 }
+
+module.exports = function solveSudoku(matrix) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (matrix[i][j] === 0) {
+        for (let x = 1; x <= 9; x++) {
+          if (isCorrect(matrix, i, j, x)) {
+            matrix[i][j] = x;
+            if (solveSudoku(matrix)) {
+              return matrix;
+            } else {
+              matrix[i][j] = 0;
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return matrix;
+};
